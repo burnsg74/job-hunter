d067e986d32635c80cd12dad395bd8a0b6723e1b
greg <greg@burnswebservices.com>
2024-10-26 13:19:29 -0700

    Job Updates

--- tests/jobs.spec.js
+++ tests/jobs.spec.js
@@ -6,14 +6,6 @@ sqlite3.verbose();
 
 test('getjobs', async ({ page }) => {
     test.setTimeout(0);
-    await page.setExtraHTTPHeaders({
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.9'
-    });
-    await page.setExtraHTTPHeaders({
-        'Accept-Language': 'en-US,en;q=0.9'
-    });
-
     const socket = io(`http://localhost:3012`, {
         withCredentials: true
     });
@@ -59,7 +51,8 @@ test('getjobs', async ({ page }) => {
 
         socket.emit('pullJobsLog', `Goto Indeed and load first page of jobs...`);
         await page.goto("https://www.indeed.com/jobs?q=" + jobTitle.replace(' ', '+') + "&l=Remote&fromage=3");
-        await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 4000 + 1000)));
+        await page.pause()
+        await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 6000 + 1000)));
         await page.evaluate(() => window.scrollBy(0, window.innerHeight));
         await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 4000 + 1000)));
 
